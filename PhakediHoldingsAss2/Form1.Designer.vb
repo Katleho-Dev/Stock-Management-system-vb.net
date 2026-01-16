fb9861108c025328d049e689a19907ce035c97eb
Katleho Nhlapo <katlehonhlapo36@gmail.com>
2026-01-16 21:12:13 +0200

    Add files via upload

---
+++ PhakediHoldingsAss2/Form1.Designer.vb
@@ -0,0 +1,318 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.txtCostprice = New System.Windows.Forms.TextBox()
+        Me.txtProdName = New System.Windows.Forms.TextBox()
+        Me.txtOrderid = New System.Windows.Forms.TextBox()
+        Me.txtQuantityO = New System.Windows.Forms.TextBox()
+        Me.txtProdId = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.btnSearch = New System.Windows.Forms.Button()
+        Me.txtSupplier = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.btnDisplay = New System.Windows.Forms.Button()
+        Me.btnInsert = New System.Windows.Forms.Button()
+        Me.btnUpdate = New System.Windows.Forms.Button()
+        Me.btnDelete = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(30, 38)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(183, 108)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Image = CType(resources.GetObject("PictureBox2.Image"), System.Drawing.Image)
+        Me.PictureBox2.Location = New System.Drawing.Point(581, 38)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(185, 108)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox2.TabIndex = 1
+        Me.PictureBox2.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(356, 57)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(111, 17)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "PHAKEDI CAFE`"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.txtCostprice)
+        Me.GroupBox1.Controls.Add(Me.txtProdName)
+        Me.GroupBox1.Controls.Add(Me.txtOrderid)
+        Me.GroupBox1.Controls.Add(Me.txtQuantityO)
+        Me.GroupBox1.Controls.Add(Me.txtProdId)
+        Me.GroupBox1.Controls.Add(Me.Label6)
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Location = New System.Drawing.Point(30, 196)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(340, 317)
+        Me.GroupBox1.TabIndex = 3
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "GroupBox1"
+        '
+        'txtCostprice
+        '
+        Me.txtCostprice.Location = New System.Drawing.Point(137, 121)
+        Me.txtCostprice.Name = "txtCostprice"
+        Me.txtCostprice.Size = New System.Drawing.Size(100, 22)
+        Me.txtCostprice.TabIndex = 9
+        '
+        'txtProdName
+        '
+        Me.txtProdName.Location = New System.Drawing.Point(137, 77)
+        Me.txtProdName.Name = "txtProdName"
+        Me.txtProdName.Size = New System.Drawing.Size(100, 22)
+        Me.txtProdName.TabIndex = 8
+        '
+        'txtOrderid
+        '
+        Me.txtOrderid.Location = New System.Drawing.Point(137, 195)
+        Me.txtOrderid.Name = "txtOrderid"
+        Me.txtOrderid.Size = New System.Drawing.Size(100, 22)
+        Me.txtOrderid.TabIndex = 7
+        '
+        'txtQuantityO
+        '
+        Me.txtQuantityO.Location = New System.Drawing.Point(137, 157)
+        Me.txtQuantityO.Name = "txtQuantityO"
+        Me.txtQuantityO.Size = New System.Drawing.Size(100, 22)
+        Me.txtQuantityO.TabIndex = 6
+        '
+        'txtProdId
+        '
+        Me.txtProdId.Location = New System.Drawing.Point(137, 38)
+        Me.txtProdId.Name = "txtProdId"
+        Me.txtProdId.Size = New System.Drawing.Size(100, 22)
+        Me.txtProdId.TabIndex = 5
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(22, 198)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(62, 17)
+        Me.Label6.TabIndex = 4
+        Me.Label6.Text = "Order ID"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(13, 160)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(118, 17)
+        Me.Label5.TabIndex = 3
+        Me.Label5.Text = "Quantity Ordered"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(22, 124)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(72, 17)
+        Me.Label4.TabIndex = 2
+        Me.Label4.Text = "Cost Price"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(22, 82)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(98, 17)
+        Me.Label3.TabIndex = 1
+        Me.Label3.Text = "Product Name"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(22, 41)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(74, 17)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "Product ID"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.btnSearch)
+        Me.GroupBox2.Controls.Add(Me.txtSupplier)
+        Me.GroupBox2.Controls.Add(Me.Label7)
+        Me.GroupBox2.Controls.Add(Me.DataGridView1)
+        Me.GroupBox2.Location = New System.Drawing.Point(390, 196)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(492, 317)
+        Me.GroupBox2.TabIndex = 4
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "GroupBox2"
+        '
+        'btnSearch
+        '
+        Me.btnSearch.Location = New System.Drawing.Point(264, 40)
+        Me.btnSearch.Name = "btnSearch"
+        Me.btnSearch.Size = New System.Drawing.Size(75, 31)
+        Me.btnSearch.TabIndex = 9
+        Me.btnSearch.Text = "Search"
+        Me.btnSearch.UseVisualStyleBackColor = True
+        '
+        'txtSupplier
+        '
+        Me.txtSupplier.Location = New System.Drawing.Point(123, 41)
+        Me.txtSupplier.Name = "txtSupplier"
+        Me.txtSupplier.Size = New System.Drawing.Size(100, 22)
+        Me.txtSupplier.TabIndex = 9
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(19, 41)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(77, 17)
+        Me.Label7.TabIndex = 5
+        Me.Label7.Text = "Supplier ID"
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(6, 124)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowTemplate.Height = 24
+        Me.DataGridView1.Size = New System.Drawing.Size(480, 173)
+        Me.DataGridView1.TabIndex = 0
+        '
+        'btnDisplay
+        '
+        Me.btnDisplay.Location = New System.Drawing.Point(30, 530)
+        Me.btnDisplay.Name = "btnDisplay"
+        Me.btnDisplay.Size = New System.Drawing.Size(75, 30)
+        Me.btnDisplay.TabIndex = 5
+        Me.btnDisplay.Text = "Display"
+        Me.btnDisplay.UseVisualStyleBackColor = True
+        '
+        'btnInsert
+        '
+        Me.btnInsert.Location = New System.Drawing.Point(121, 527)
+        Me.btnInsert.Name = "btnInsert"
+        Me.btnInsert.Size = New System.Drawing.Size(75, 32)
+        Me.btnInsert.TabIndex = 6
+        Me.btnInsert.Text = "Insert"
+        Me.btnInsert.UseVisualStyleBackColor = True
+        '
+        'btnUpdate
+        '
+        Me.btnUpdate.Location = New System.Drawing.Point(222, 526)
+        Me.btnUpdate.Name = "btnUpdate"
+        Me.btnUpdate.Size = New System.Drawing.Size(75, 31)
+        Me.btnUpdate.TabIndex = 7
+        Me.btnUpdate.Text = "Update"
+        Me.btnUpdate.UseVisualStyleBackColor = True
+        '
+        'btnDelete
+        '
+        Me.btnDelete.Location = New System.Drawing.Point(327, 524)
+        Me.btnDelete.Name = "btnDelete"
+        Me.btnDelete.Size = New System.Drawing.Size(75, 35)
+        Me.btnDelete.TabIndex = 8
+        Me.btnDelete.Text = "Delete"
+        Me.btnDelete.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(968, 700)
+        Me.Controls.Add(Me.btnDelete)
+        Me.Controls.Add(Me.btnUpdate)
+        Me.Controls.Add(Me.btnInsert)
+        Me.Controls.Add(Me.btnDisplay)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox2 As System.Windows.Forms.PictureBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
+    Friend WithEvents txtCostprice As System.Windows.Forms.TextBox
+    Friend WithEvents txtProdName As System.Windows.Forms.TextBox
+    Friend WithEvents txtOrderid As System.Windows.Forms.TextBox
+    Friend WithEvents txtQuantityO As System.Windows.Forms.TextBox
+    Friend WithEvents txtProdId As System.Windows.Forms.TextBox
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents btnSearch As System.Windows.Forms.Button
+    Friend WithEvents txtSupplier As System.Windows.Forms.TextBox
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents btnDisplay As System.Windows.Forms.Button
+    Friend WithEvents btnInsert As System.Windows.Forms.Button
+    Friend WithEvents btnUpdate As System.Windows.Forms.Button
+    Friend WithEvents btnDelete As System.Windows.Forms.Button
+
+End Class
---
+++ PhakediHoldingsAss2/Form1.vb
@@ -0,0 +1,143 @@
+﻿Public Class Form1
+    '0607090260086
+    'Setting the provider and the data source for DB connection
+    Dim strProvider As String = "Provider=Microsoft.ACE.OLEDB.12.0;"
+    Dim strDataSource As String = "Data Source=C:\Users\katle\OneDrive\Documents\PhakediAss2.accdb"
+
+    Dim strFilepath As String = strProvider & strDataSource
+    Dim bs As BindingSource
+
+    Private Sub btnDisplay_Click(sender As Object, e As EventArgs) Handles btnDisplay.Click
+        'Establishing the DB connection
+        Dim conn As New OleDb.OleDbConnection
+        Dim command As New OleDb.OleDbCommand
+        Dim table As New Data.DataTable
+        Dim strSQL As String = ""
+        Try
+            conn.ConnectionString = strFilepath
+            conn.Open()
+            command.Connection = conn
+            strSQL = "SELECT * FROM Products;"
+            command.CommandText = strSQL
+            table.Load(command.ExecuteReader())
+            conn.Close()
+            DataGridView1.DataSource = table
+
+        Catch ex As Exception
+
+        End Try
+
+    End Sub
+
+    Private Sub btnInsert_Click(sender As Object, e As EventArgs) Handles btnInsert.Click
+
+        'Defensive programming
+        If String.IsNullOrWhiteSpace(txtProdName.Text) Then
+            MessageBox.Show("Please fill in all fields before continuing", "Missing Data", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+        End If
+        Dim conn As New OleDb.OleDbConnection
+        Dim comm As New OleDb.OleDbCommand
+        Try
+            conn.ConnectionString = strFilepath
+            conn.Open()
+            comm.Connection = conn
+            'Sql Statement
+            comm.CommandText = "INSERT INTO [Order] (OrderID, OrderQuantity) VALUES (@OrderID, @OrderQuantity)"
+            comm.Parameters.AddWithValue("@OrderQuantity", txtQuantityO.Text)
+            comm.Parameters.AddWithValue("@OrderID", txtOrderid.Text)
+            'Execute Insert
+            comm.ExecuteNonQuery()
+            MessageBox.Show("Order Inserted Successfully.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            'Reload data into the datagrid
+            Dim table As New DataTable
+            comm.CommandText = "SELECT * FROM [Order]"
+            comm.Parameters.Clear()
+            Dim adapter As New OleDb.OleDbDataAdapter(comm)
+            adapter.Fill(table)
+            DataGridView1.DataSource = table
+        Catch ex As Exception
+            MessageBox.Show("Error" & ex.Message, "Insert Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+    Private Sub btnUpdate_Click(sender As Object, e As EventArgs) Handles btnUpdate.Click
+        Dim conn As New OleDb.OleDbConnection
+        Dim table As New Data.DataTable
+        Dim command As New OleDb.OleDbCommand
+        Dim strsql As String = ""
+        Try
+            conn.ConnectionString = strFilepath
+            conn.Open()
+            command.Connection = conn
+            strsql = "UPDATE [Products] SET" & "ProductName = @ProductName, CostPrice = @CostPrice" & "WHERE ProductID = @ProductID"
+            command.CommandText = strsql
+            command.Parameters.AddWithValue("@ProductName", txtProdName.Text)
+            command.Parameters.AddWithValue("@CostPrice", txtCostprice.Text)
+            command.Parameters.AddWithValue("@ProductID", txtProdId.Text)
+
+            Dim rowsAffected As Integer = command.ExecuteNonQuery()
+            If rowsAffected > 0 Then
+                MessageBox.Show("Product updated successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            End If
+            Dim adapter = New OleDb.OleDbDataAdapter("SELECT * FROM [Products]", conn)
+            table.Load(command.ExecuteReader())
+            adapter.Fill(table)
+            DataGridView1.DataSource = table
+
+            conn.Close()
+        Catch ex As Exception
+            MessageBox.Show("Error: " & ex.Message, "Update Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+
+    End Sub
+
+    Private Sub btnDelete_Click(sender As Object, e As EventArgs) Handles btnDelete.Click
+        Dim conn As New OleDb.OleDbConnection
+        Dim command As New OleDb.OleDbCommand
+        Dim table As New Data.DataTable
+        Dim strSQl As String = ""
+        Try
+            conn.ConnectionString = strFilepath
+            conn.Open()
+            command.Connection = conn
+            strSQl = "DELETE FROM Supplier WHERE SupplierID = 1;"
+            command.CommandText = strSQl
+            table.Load(command.ExecuteReader())
+            conn.Close()
+            DataGridView1.DataSource = table
+
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub btnSearch_Click(sender As Object, e As EventArgs) Handles btnSearch.Click
+        'Defensive Programming
+        If String.IsNullOrWhiteSpace(txtSupplier.Text) Then
+            MessageBox.Show("Please enter Supplier Id to search", "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            Exit Sub
+        End If
+        'Declare connection, command and data table
+        Dim conn As New OleDb.OleDbConnection
+        Dim command As New OleDb.OleDbCommand
+        Dim table As New DataTable
+        Try
+            conn.ConnectionString = strFilepath
+            conn.Open()
+            command.Connection = conn
+            command.CommandText = "SELECT * FROM Supplier WHERE SupplierID =?;"
+            command.Parameters.AddWithValue("?", txtSupplier.Text)
+            'Load Results
+            table.Load(command.ExecuteReader)
+            conn.Close()
+            If table.Rows.Count = 0 Then
+                MessageBox.Show("Supplier Not Found", "Not Found", MessageBoxButtons.RetryCancel, MessageBoxIcon.Information)
+            End If
+            'Show results in DataGrid
+            DataGridView1.DataSource = table
+
+        Catch ex As Exception
+
+        End Try
+    End Sub
+End Class
